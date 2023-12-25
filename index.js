1682766444766840e262aecbc395b2c9b856c16c
devLateef <lateefolatayo@gmail.com>
2023-12-25 22:14:56 +0100

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express()
+const port = process.env.PORT || 8001;
+
+app.get('/', (req, res)=>{
+    res.json('Welcome to my application');
+});
+
+
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}`);
+});
