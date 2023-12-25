d590e2b81511b5f4543eeadf573eb51d721b335c
devLateef <lateefolatayo@gmail.com>
2023-12-25 22:29:22 +0100

    Added more fields to user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,34 @@
 const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
-    name: {
+    firstname: {
         type: String,
         require: true
-    }
-})
+    },
+    surname: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    referral: {
+        type: String,
+        require: true
+    },
+},
+{
+    timestamp: true
+});
+
+const userCollection = mongoose.model('User', userSchema);
+
+module.exports = userCollection;
